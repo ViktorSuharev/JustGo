48445cbaacf4d133aacb3170ed0a38a7dd09c600
ViktorSuharev <viktor.suhareff@yandex.ru>
2017-10-27 22:51:48 +0300

    add jdbc connector

---
+++ src/main/java/com/webapp/mvcapp/dbmanagers/PostgresJdbc.java
@@ -0,0 +1,21 @@
+package com.webapp.mvcapp.dbmanagers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class PostgresJdbc {
+    public static void main(String args[]) {
+        Connection c = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/postgres",
+                            "postgres", "postgres");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+}
